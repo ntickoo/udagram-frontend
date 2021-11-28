4d9615d2a5f88b9ba4c9c81ee2d5aa7904ab60a2
Neeraj Tickoo <ntickoo@gmail.com>
2021-11-27 23:01:05 -0500

    Add resource limits to pods. Make URL a environment variable for Reverseproxy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+let url: string = process.env.URL ||  'http://localhost:8080/api/v0'
+
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: url
 };
 
 /*
