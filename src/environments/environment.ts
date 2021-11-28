fcff253f4480a11f8953e3adc9639dcfd520138b
Neeraj Tickoo <ntickoo@gmail.com>
2021-11-27 23:44:07 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,10 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-let url: string = process.env.URL ||  'http://localhost:8080/api/v0'
-
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: url
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
